f677458ff6b8d2278a25b8e72e4a94059924082e
EKKO-LING\2863495231 <ekko>
2022-08-24 00:02:26 +0800

    css浏览器兼容处理及跨越代理处理

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 import { Button } from 'antd'
+import axios from 'axios'
 export default class index extends Component {
+
+  componentDidMount() {
+    // axios.get('/api/v1/getUser').then((res) => { console.log(res)}, (err) => {
+    //   console.log(err)
+    // })
+  }
   render() {
     return (
       <div id='login'>
